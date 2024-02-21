9dc87ef41980e981cb812f287689bb6fd85d4cfa
LUIS <luis.enrique.enc@gmail.com>
2024-02-21 09:25:27 -0600

    [ADD] se agrega array de imágenes y el timer con la función para presentar imágenes random

--- CardWorkout/CardSelectionViewController.swift
+++ CardWorkout/CardSelectionViewController.swift
@@ -10,18 +10,29 @@ import UIKit
 class CardSelectionViewController: UIViewController {
     
   
+    @IBOutlet var cardImageView: UIImageView!
     @IBOutlet var buttons: [UIButton]!
     
-    
+    var timer : Timer!
+    var cards : [UIImage] = Deck.allValues
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        startTimer()
         for button in buttons {
             button.layer.cornerRadius = 8
         }
     }
 
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(showRandomImage), userInfo: nil, repeats: true)
+    }
+    
+    @objc func showRandomImage() {
+        cardImageView.image = cards.randomElement() ?? UIImage(named: "AS")
+    }
+    
+    
     @IBAction func stopButtonTapped(_ sender: Any) {
         
     }
